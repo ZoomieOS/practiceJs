55a472a94032c2940682bc5edeed4aaec067d45c
ZoomieOS <iwenik@gmail.com>
2020-09-22 21:55:13 +0300

    Refactore code

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-let budgetOnTheMonth = +prompt('Ваш бюджет на месяц?'),
-    periodMoney = prompt('Введите дату в формате YYYY-MM-DD'),
-    firstQuestion = prompt('Введите обязательную статью расходов в этом месяце'),
-    secondQuestion = prompt('Во сколько обойдется?');
+let budgetOnTheMonth = +prompt("Ваш бюджет на месяц?"),
+  periodMoney = prompt("Введите дату в формате YYYY-MM-DD"),
+  firstQuestion = prompt("Введите обязательную статью расходов в этом месяце"),
+  secondQuestion = prompt("Во сколько обойдется?");
 
-let budgetText = document.querySelector('#budget');
+let budgetText = document.querySelector("#budget");
 
 const appData = {
-    budgetOnTheMonth,
-    timeData: periodMoney,
-    expenses: {
-        firstQuestion: secondQuestion
-    },
-    optionalExpenses: {},
-    income: [],
-    savings: false,
-    budgetOfTheDay: function() {
-        budgetText.innerHTML = `My budget is: ${this.budgetOnTheMonth / 30 }`;
-    }
+  budgetOnTheMonth,
+  timeData: periodMoney,
+  expenses: {
+    firstQuestion: secondQuestion,
+  },
+  optionalExpenses: {},
+  income: [],
+  savings: false,
+  budgetOfTheDay: function () {
+    this.budgetOnTheMonth > 0
+      ? (budgetText.innerHTML = `My budget is: ${this.budgetOnTheMonth / 30}`)
+      : (budgetText.innerHTML = "Budget NOT found!!!");
+  },
 };
 
-appData.budgetOfTheDay();   
-
+appData.budgetOfTheDay();
