932be5522e1b080c837f433af9be918cad188d57
ZoomieOS <iwenik@gmail.com>
2020-09-22 23:19:35 +0300

    Budget add function check

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-let budgetOnTheMonth = +prompt("Ваш бюджет на месяц?"),
-    periodMoney = prompt("Введите дату в формате YYYY-MM-DD");
+let budgetOnTheMonth,
+  periodMoney,
+  budgetText = document.querySelector("#budget");
 
-let budgetText = document.querySelector("#budget");
+(function () {
+  (budgetOnTheMonth = +prompt("Ваш бюджет на месяц?")),
+    (periodMoney = prompt("Введите дату в формате YYYY-MM-DD"));
+
+  while (
+    isNaN(budgetOnTheMonth) ||
+    budgetOnTheMonth == "" ||
+    budgetOnTheMonth == null
+  ) {
+    budgetOnTheMonth = +prompt("Ваш бюджет на месяц?");
+  }
+})();
 
 const appData = {
   budgetOnTheMonth,
@@ -12,47 +24,58 @@ const appData = {
   savings: false,
   budgetOfTheDay: function (dayOfMonth = 30) {
     this.budgetOnTheMonth > 0
-      ? (budgetText.innerHTML = `My budget is: ${this.budgetOnTheMonth / dayOfMonth}`)
+      ? (budgetText.innerHTML = `My budget is: ${(
+          this.budgetOnTheMonth / dayOfMonth
+        ).toFixed()}`)
       : (budgetText.innerHTML = "Budget NOT found!!!");
   },
   detectLevel: function () {
-    if(this.budgetOnTheMonth < 100 ) {
-      console.log('Низкий достаток')
+    if (this.budgetOnTheMonth < 100) {
+      console.log("Низкий достаток");
     } else if (this.budgetOnTheMonth < 1000) {
-      console.log('Средний достаток')
+      console.log("Средний достаток");
     } else if (this.budgetOnTheMonth < 10000) {
-      console.log('Высокий достаток');
+      console.log("Высокий достаток");
     }
   },
-  chooseOptExpenses: function() {
-    for(let i = 1; i < 4; i++) {
-      let opt = prompt('Статья необязательных расходов?');
+  chooseOptExpenses: function () {
+    for (let i = 1; i < 4; i++) {
+      let opt = prompt("Статья необязательных расходов?");
       this.optionalExpenses[i] = opt;
     }
-  }
+  },
+  chooseExpenses: function () {
+    for (let i = 0; i < 2; i++) {
+      let firstQuestion = prompt(
+          "Введите обязательную статью расходов в этом месяце"
+        ),
+        secondQuestion = prompt("Во сколько обойдется?");
+
+      if (
+        typeof firstQuestion === "string" &&
+        typeof firstQuestion != null &&
+        typeof secondQuestion &&
+        firstQuestion != "" &&
+        secondQuestion != "" &&
+        firstQuestion.length < 50
+      ) {
+        appData.expenses[firstQuestion] = secondQuestion;
+      } else {
+      }
+    }
+  },
+  checkSavings: function () {
+    if (this.savings === true) {
+      let save = +prompt("Какова сумма накоплений?"),
+        percent = +prompt("Под какой процент?");
+      this.monthIncome = (save / 100 / 12) * percent;
+      alert(`Доход в месяц от вашего депозита:${this.monthIncome}`);
+    }
+  },
 };
-
-for (let i = 0; i < 2; i++) {
-  let firstQuestion = prompt(
-      "Введите обязательную статью расходов в этом месяце"
-    ),
-    secondQuestion = prompt("Во сколько обойдется?");
-
-  if (
-    typeof firstQuestion === "string" &&
-    typeof firstQuestion != null &&
-    typeof secondQuestion &&
-    firstQuestion != "" &&
-    secondQuestion != "" &&
-    firstQuestion.length < 50
-  ) {
-    appData.expenses[firstQuestion] = secondQuestion;
-  } else {
-
-  }
-};
-
 
 appData.budgetOfTheDay();
+appData.chooseExpenses();
 appData.detectLevel();
 appData.chooseOptExpenses();
+appData.checkSavings();
