d67f4a5a60e14d754d7434e9008a11be48f3df92
ZoomieOS <iwenik@gmail.com>
2020-09-22 23:04:58 +0300

    Add some functions detectLevel and chooseOptExpenses

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let budgetOnTheMonth = +prompt("Ваш бюджет на месяц?"),
-  periodMoney = prompt("Введите дату в формате YYYY-MM-DD");
+    periodMoney = prompt("Введите дату в формате YYYY-MM-DD");
 
 let budgetText = document.querySelector("#budget");
 
@@ -10,18 +10,49 @@ const appData = {
   optionalExpenses: {},
   income: [],
   savings: false,
-  budgetOfTheDay: function () {
+  budgetOfTheDay: function (dayOfMonth = 30) {
     this.budgetOnTheMonth > 0
-      ? (budgetText.innerHTML = `My budget is: ${this.budgetOnTheMonth / 30}`)
+      ? (budgetText.innerHTML = `My budget is: ${this.budgetOnTheMonth / dayOfMonth}`)
       : (budgetText.innerHTML = "Budget NOT found!!!");
   },
+  detectLevel: function () {
+    if(this.budgetOnTheMonth < 100 ) {
+      console.log('Низкий достаток')
+    } else if (this.budgetOnTheMonth < 1000) {
+      console.log('Средний достаток')
+    } else if (this.budgetOnTheMonth < 10000) {
+      console.log('Высокий достаток');
+    }
+  },
+  chooseOptExpenses: function() {
+    for(let i = 1; i < 4; i++) {
+      let opt = prompt('Статья необязательных расходов?');
+      this.optionalExpenses[i] = opt;
+    }
+  }
 };
 
-appData.budgetOfTheDay();
+for (let i = 0; i < 2; i++) {
+  let firstQuestion = prompt(
+      "Введите обязательную статью расходов в этом месяце"
+    ),
+    secondQuestion = prompt("Во сколько обойдется?");
 
-for(let i = 0; i < 2; i++) {
-    let firstQuestion = prompt("Введите обязательную статью расходов в этом месяце"),
-        secondQuestion = prompt("Во сколько обойдется?");
-    
-    appData.expenses[firstQuestion] = secondQuestion;    
-}
+  if (
+    typeof firstQuestion === "string" &&
+    typeof firstQuestion != null &&
+    typeof secondQuestion &&
+    firstQuestion != "" &&
+    secondQuestion != "" &&
+    firstQuestion.length < 50
+  ) {
+    appData.expenses[firstQuestion] = secondQuestion;
+  } else {
+
+  }
+};
+
+
+appData.budgetOfTheDay();
+appData.detectLevel();
+appData.chooseOptExpenses();
---
+++ common.js
@@ -0,0 +1,5 @@
+function showFirstMessage(text = 'Oleg') {
+    alert(text);
+}
+
+showFirstMessage();
