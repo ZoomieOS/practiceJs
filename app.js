202a3ab28bc525510f1a245ac733cbc627b7eb65
ZoomieOS <iwenik@gmail.com>
2020-09-24 22:19:36 +0300

    Event delegation and animation

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-let timerId = setTimeout(sayHello, 1000);
-clearTimeout(timerId);
+let btnAnimate = document.querySelector('.btn'),
+    box = document.querySelector('.box');
 
-function sayHello() {
-  alert("Привет!");
+function animationBox() {
+  let pos = 0,
+      idPos = setInterval(frame, 10);
+
+  function frame() {
+    if(pos === 300) {
+      clearInterval(idPos);
+    } else {
+      pos++;
+      box.style.top = pos + 'px';
+      box.style.left = pos + 'px';
+    }
 }
+};
+
+btnAnimate.addEventListener('click', animationBox);
+
+
+let btnBlock = document.querySelector('.btn-block'),
+    btn = document.getElementsByTagName('button');
+
+btnBlock.addEventListener('click', function(event) {
+    if (event.target && event.target.matches('button.clickBtn')) {
+      console.log("Hello!");
+    }
+});
